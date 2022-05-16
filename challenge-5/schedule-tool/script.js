6937a10f1fcc4973da23eb6a65492f8fee6dc913
Eric Lake <ericallenlake@gmail.com>
2022-05-15 18:30:16 -0600

    Adds input functionality
    Starts to add input functionality

--- challenge-5/schedule-tool/script.js
+++ challenge-5/schedule-tool/script.js
@@ -1,15 +1,14 @@
 // Sets the time at the top of the page
 let current = $("#currentDay");
+let timeNow;
 
-
-let timeNow = 15;
-
-
-// setInterval(function(){
-//     current.text(moment().format('dddd, MMM Do. h:mm:ss a'));
-//     timeNow = moment().format('HH.mm');
-//     console.log(timeNow);
-// }, 1000)
+// Updates time every second
+setInterval(function(){
+    current.text(moment().format('dddd, MMM Do. h:mm:ss a'));
+    timeNow = moment().format('HH');
+    timeNow = 15
+    update();
+}, 1000)
 
 // Accesses the rows by time
 let nineAM = $("#09");
@@ -22,17 +21,46 @@ let threePM = $('#15');
 let fourPM = $('#16');
 let fivePM = $('#17');
 let latePM = $('#23');
-
 let workDay = [nineAM, tenAM, elevenAM, twelvePM, onePM, twoPM, threePM, fourPM, fivePM, latePM];
 
-workDay.forEach( element => {
-    if (element[0].id < timeNow){
-        element[0].classList.add('past')
-        console.log(element[0].id)
-    } else if (element[0].id == timeNow ){
-        element[0].classList.add('current')
-    } else element[0].classList.add('future')
-    element[0].addEventListener('click', function(){
-        console.log(element[0].id)
+// Updates time blocks based on current time
+let update = () => {
+    workDay.forEach( element => {
+        // past times
+        if (element[0].id < timeNow){
+            element[0].classList.add('past')
+            if (element[0].classList.contains('current')) {
+                element[0].classList.remove('current')
+            }
+            if (element[0].classList.contains('future')) {
+                element[0].classList.remove('future');
+            }
+        } 
+        // current time
+        else if (element[0].id == timeNow ){
+            element[0].classList.add('current')
+            if (element[0].classList.contains('future')) {
+                element[0].classList.remove('future')
+            }
+            if (element[0].classList.contains('past')){
+                element[0].classList.remove('past');
+            }
+        } 
+        // future
+        else {
+            element[0].classList.add('future');
+            if (element[0].classList.contains('current')){
+                element[0].classList.remove('current')
+            }
+        }
     })
+}
+
+
+workDay.forEach(element => {
+    element[0].addEventListener('click', function(){
+        let input = document.createElement("input")
+        element[0].children[1].append(input)
+        console.log(element[0].children[1])
+    })  
 })
